5b8b6efea3417ecf2a96dba950d7d2046982aca4
Christian Winther <jippignu@gmail.com>
2024-01-30 00:12:43 +0100

    please linter

--- cmd/cmd_json.go
+++ cmd/cmd_json.go
@@ -12,7 +12,7 @@ var jsonCommand = &cli.Command{
 	Name:   "json",
 	Usage:  "Print as JSON",
 	Before: setup,
-	Action: func(_ context.Context, cmd *cli.Command) error {
+	Action: func(_ context.Context, _ *cli.Command) error {
 		b, err := json.MarshalIndent(env, "", "  ")
 		if err != nil {
 			return err
