dcdbb1ebbc6e4f0d6dfed5430f46d0c94b6e87d3
Christian Winther <jippignu@gmail.com>
2024-01-31 00:48:06 +0100

    sync

--- pkg/token/annotation.go
+++ pkg/token/annotation.go
@@ -1,6 +1,19 @@
 package token
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Annotation struct {
 	Key   string
 	Value string
+}
+
+func (a Annotation) String() string {
+	return fmt.Sprintf("ANNOTATION(KEY=%s;VALUE=%s)", a.Key, a.Value)
+}
+
+func (a Annotation) IsDottie() bool {
+	return strings.HasPrefix(a.Key, "dottie/")
 }
