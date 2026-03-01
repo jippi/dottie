f0392219f8a7e39d3b51f688592701f5a8eda72b
Christian Winther <jippignu@gmail.com>
2026-03-01 20:16:42 +0100

    test(shell): add arg and command wiring tests

--- cmd/shell/shell_test.go
+++ cmd/shell/shell_test.go
@@ -1,13 +1,53 @@
 package shell_test
 
 import (
+	"bytes"
 	"testing"
 
-	"github.com/jippi/dottie/pkg/test_helpers"
+	"github.com/jippi/dottie/cmd/shell"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestShellCommand(t *testing.T) {
+func TestShellCommandMetadata(t *testing.T) {
 	t.Parallel()
 
-	test_helpers.RunFileBasedCommandTests(t, 0, "shell")
+	command := shell.New()
+
+	assert.Equal(t, "shell", command.Use)
+	assert.Equal(t, "manipulate", command.GroupID)
+	require.NotNil(t, command.Args)
+	require.NotNil(t, command.RunE)
+}
+
+func TestShellCommandArgsValidation(t *testing.T) {
+	t.Parallel()
+
+	command := shell.New()
+
+	require.NoError(t, command.Args(command, []string{}))
+
+	err := command.Args(command, []string{"unexpected"})
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "accepts 0 arg(s), received 1")
+}
+
+func TestShellCommandExecuteRejectsExtraArgs(t *testing.T) {
+	t.Parallel()
+
+	var stdout bytes.Buffer
+
+	var stderr bytes.Buffer
+
+	command := shell.New()
+	command.SetOut(&stdout)
+	command.SetErr(&stderr)
+	command.SetArgs([]string{"unexpected"})
+
+	err := command.Execute()
+
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "accepts 0 arg(s), received 1")
+	assert.Equal(t, "shell", command.Name())
+	assert.Contains(t, stderr.String(), "accepts 0 arg(s), received 1")
 }
